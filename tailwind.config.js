0a052587cdc5a62653010738837e1285426bbba8
jnakht <jisan.cse.cu@gmail.com>
2025-02-05 12:33:05 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Ensure the correct file paths
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+};
